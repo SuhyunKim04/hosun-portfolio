3e0c49835fbb0969df5d09077abe911b796d7874
Suhyun Kim <kim00431@algonquinlive.com>
2023-08-29 17:29:29 -0400

    updates

--- js/common.js
+++ js/common.js
@@ -2,10 +2,12 @@ const hosun = () => {
     const contents = document.querySelector(".contents");
     const box = document.querySelector(".box");
     const pics = contents.querySelector(".pics");
+    const click = contents.querySelector(".click");
 
     pics.addEventListener('click',(e) => {
         console.log('click')
         box.classList.toggle('open')
+        click.classList.toggle('close');
     })
 }
 
