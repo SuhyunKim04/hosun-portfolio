35ff2c7f91c5ba76ed1582077cc0778595fc6418
Suhyun Kim <kim00431@algonquinlive.com>
2023-09-02 22:09:03 -0400

    updates

--- js/common.js
+++ js/common.js
@@ -1,5 +1,8 @@
 const hosun = () => {
     const contents = document.querySelector(".contents");
+    if(!contents){
+        return false;
+    }
     const box = document.querySelector(".box");
     const pics = contents.querySelector(".pics");
     const click = contents.querySelector(".click");
@@ -9,6 +12,39 @@ const hosun = () => {
         box.classList.toggle('open')
         click.classList.toggle('close');
     })
+    
 }
 
+const height = () =>{
+    const btn = document.querySelector('.top');
+    let doc = document.documentElement;
+    window.addEventListener('scroll',() =>{
+       
+        if(window.pageYOffset > 500) {
+            btn.classList.add('visible')
+        }else{
+            btn.classList.remove('visible')
+        }
+
+        btn.addEventListener('click', (e) =>{
+            console.log('test')
+            e.preventDefault();
+            doc.scrollTop = 0;
+            
+        })
+    })
+}
+    // var btn = document.querySelector(".top");
+    // doc = document.documentElement,
+    // // offset,
+    // // scollPos,
+    // // docHeight;
+
+    // docHeight = doc.offsetHeight;
+    // scrollPos = doc.scrollTop;
+    // console.log(scrollPos);
+
+
+height();
 hosun();
+
