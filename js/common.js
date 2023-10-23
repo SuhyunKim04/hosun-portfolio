9c47d1546eade8f756a3f5ca3bebccf3a134f329
Suhyun Kim <kim00431@algonquinlive.com>
2023-10-23 17:25:43 -0400

    updates

--- js/common.js
+++ js/common.js
@@ -29,6 +29,35 @@ const height = () =>{
 }
 
 
+
+const MobileM = () => {
+    const ham = document.querySelector('.ham_menu');
+    const close = document.querySelector('.close_menu');
+    const dimm = document.querySelector('.dimm');
+    const gnb = document.querySelector('.gnb');
+
+    console.log(dimm)
+    
+    ham.addEventListener('click', (e) =>{
+        e.currentTarget.classList.add('open');
+        let status = e.currentTarget.classList.contains('open')
+        if(status) {
+            gnb.style.display = 'block';
+            gnb.style.color = 'blue'
+            dimm.style.display='block'
+            close.style.display='block'
+        }
+    })
+
+    close.addEventListener('click', (e) => {
+        close.classList.remove('open')
+        dimm.style.display = 'none'
+        gnb.style.display = 'none'
+    })
+
+}
+
+
 height();
 hosun();
-
+MobileM();
